028612e7d10cc7b0b83d9498b6df4802dbd68e79
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2018-12-01 20:59:38 +0200

    Add parameters to the timer, make it actually work

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,53 @@
 #include <chrono>
 #include <thread>
 
-namespace {
 const std::chrono::minutes default_work_interval{25};
 const std::chrono::minutes default_break_interval{5};
+
+static void show_usage(const std::string& name)
+{
+    std::cerr << name << " - Pomodoro timer." << std::endl;
+    std::cerr << "Usage: " << name << " [<options>]" << std::endl;
+    std::cerr << R"(
+Options:
+
+-w | --work-interval    Work interval in minutes
+-b | --break-interval   Break interval in minutes
+-h | --help             Show this help message)" << std::endl;
 }
 
 int main(int argc, char** argv)
 {
     std::cout << "Hi there! I am a pomodoro timer." << std::endl;
 
-    //auto start{std::chrono::system_clock::now()};
+    auto work_interval{default_work_interval};
+    auto break_interva{default_break_interval};
 
-    std::cout << "argc: " << argc << std::endl;
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
-        std::cout << "argv: " << argv[i] << std::endl;
+        std::string arg{argv[i]};
+        if (arg == "-w" || arg == "--work-interval")
+        {
+            work_interval = std::chrono::minutes{std::stoi(argv[i + 1])};
+        }
+        else if (arg == "-b" || arg == "--break-interval")
+        {
+            break_interva = std::chrono::minutes{std::stoi(argv[i + 1])};
+        }
+        else if (arg == "--help" || arg == "-h")
+        {
+            show_usage(argv[0]);
+        }
     }
 
-    // start the timer
-    std::this_thread::sleep_for(default_work_interval);
+    while (true)
+    {
+        std::cout << "Work interval has started. You have " << work_interval.count()
+                  << " min to work."  << std::endl;
+        std::this_thread::sleep_for(work_interval);
 
-    std::cout << "The time span has passed. It's time to relax." << std::endl;
+        std::cout << "It's time to relax. You have " << break_interva.count() << " min to relax."
+                  << std::endl;
+        std::this_thread::sleep_for(break_interva);
+    }
 }
